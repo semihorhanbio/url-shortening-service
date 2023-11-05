4ae06174e645f36d82d558440088d9ef7e024a3f
Semih Orhan <sorhan3.14@gmail.com>
2023-11-05 08:15:00 +0300

    fix error for getting blog post

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
-export default async function GET() {
+export async function GET() {
   return NextResponse.json({ items: [{ id: 1, title: "Hello World" }] });
 }
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,11 +1,11 @@
-const getData = async () => {
+async function getData() {
   const endpoint = "http://localhost:3000/api/posts";
   const res = await fetch(endpoint);
 
   if (!res.ok) throw new Error("Failed to fetch data");
 
   return res.json();
-};
+}
 
 export default async function BlogPage() {
   const data = await getData();
