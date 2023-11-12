8f81998ac60dfc2af53d2ca37388f23d7be8e522
Semih Orhan <sorhan3.14@gmail.com>
2023-11-12 15:20:31 +0300

    add url validation to links form

--- src/app/api/links/route.js
+++ src/app/api/links/route.js
@@ -1,13 +1,25 @@
 import { NextResponse } from "next/server";
+import isValidURL from "@/app/lib/isValidUrl";
 
 export async function POST(request) {
   const contentType = await request.headers.get("content-type");
   if (contentType !== "application/json") {
     return NextResponse.json(
       { error: "Invalid content type" },
-      { status: 400 }
+      { status: 415 }
     );
   }
   const data = await request.json();
+  const url = data && data.url ? data.url : null;
+  const validURL = await isValidURL(url, [
+    "url-shortening-service-peach.vercel.app",
+    process.env.NEXT_PUBLIC_VERCEL_URL,
+  ]);
+  if (!validURL) {
+    return NextResponse.json(
+      { message: `${url} is not valid.` },
+      { status: 400 }
+    );
+  }
   return NextResponse.json(data, { status: 201 });
 }
