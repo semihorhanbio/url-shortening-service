e26bf8ca88ba219d2b80a7e7a05607c395fbd04a
Semih Orhan <sorhan3.14@gmail.com>
2023-11-14 20:01:01 +0300

    create table wirh drizzle

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -8,3 +8,14 @@ export async function helloWorld() {
   const end = new Date();
   return { dbNow: dbNow, latency: Math.abs(end - start) };
 }
+
+async function configureDatabase() {
+  const dbResponse = await sql`CREATE TABLE IF NOT EXISTS "links" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "url" text NOT NULL,
+    "short" varchar(50),
+    "created_at" timestamp DEFAULT now()
+  );`;
+}
+
+configureDatabase().catch((err) => console.log("db config error", err));
